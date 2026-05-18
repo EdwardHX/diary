a2fcfb15b87022966ca5301d99a9413923f854e8
EdwardHX <100274157+EdwardHX@users.noreply.github.com>
2026-05-18 13:54:33 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@ release = 'v1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [sphinx_rtd_theme]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,7 +24,7 @@ language = 'zh_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['Cpp/_static']
 source_suffix = {
     '.rst': 'restructuredtext',
